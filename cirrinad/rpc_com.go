20a0dc1aa8748a9a4ca3df03259bd4ce175e8748
Steve Wills <steve@mouf.net>
2023-06-03 21:48:00 -0400

    implement serial logging

--- cirrinad/rpc_com.go
+++ cirrinad/rpc_com.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"cirrina/cirrina"
+	"cirrina/cirrinad/config"
 	"cirrina/cirrinad/vm"
 	"errors"
 	"golang.org/x/exp/slog"
 	"io"
+	"os"
 )
 
 func (s *server) Com1Interactive(stream cirrina.VMInfo_Com1InteractiveServer) error {
@@ -30,41 +32,86 @@ func (s *server) Com1Interactive(stream cirrina.VMInfo_Com1InteractiveServer) er
 	if vmInst.Com1 == nil {
 		return errors.New("com not available")
 	}
+	if vmInst.Config.Com1Log {
+		com1Chan := make(chan byte)
+		vmInst.Com1rchan = com1Chan
+		defer func() { vmInst.Com1rchan = nil }()
+	}
 
 	vmInst.Com1lock.Lock()
 	defer vmInst.Com1lock.Unlock()
 
 	slog.Debug("Com1Interactive", "vm_id", vmid.Value)
 	go func(vmInst *vm.VM, stream cirrina.VMInfo_Com1InteractiveServer) {
+		b := make([]byte, 1)
 		for {
 			if vmInst.Com1 == nil {
+				if vmInst.Config.Com1Log {
+					<-vmInst.Com1rchan
+					vmInst.Com1rchan = nil
+				}
 				return
 			}
 			if vmInst.Status != "RUNNING" {
+				if vmInst.Config.Com1Log {
+					<-vmInst.Com1rchan
+					vmInst.Com1rchan = nil
+				}
 				return
 			}
-			b := make([]byte, 1)
-			_, err := vmInst.Com1.Read(b)
-			if err == io.EOF {
-				return
-			}
-			if err != nil {
-				return
-			}
-			req := cirrina.ComDataResponse{
-				ComOutBytes: b,
-			}
+			// get byte from channel if logging, else read port directly
+			if vmInst.Config.Com1Log {
+				var b2 byte
+				b2 = <-vmInst.Com1rchan
+				b[0] = b2
+				req := cirrina.ComDataResponse{
+					ComOutBytes: b,
+				}
 
-			err = stream.Send(&req)
-			if err != nil {
-				return
+				err = stream.Send(&req)
+				if err != nil {
+					<-vmInst.Com1rchan
+					vmInst.Com1rchan = nil
+					return
+				}
+			} else {
+				_, err := vmInst.Com1.Read(b)
+				if err == io.EOF {
+					return
+				}
+				if err != nil {
+					return
+				}
+				req := cirrina.ComDataResponse{
+					ComOutBytes: b,
+				}
+
+				err = stream.Send(&req)
+				if err != nil {
+					return
+				}
 			}
 		}
 	}(vmInst, stream)
 
 	//slog.Debug("starting loop")
-	for {
+	com1LogPath := config.Config.Disk.VM.Path.State + "/" + vmInst.Name + "/"
+	com1LogFile := com1LogPath + "com1_in.log"
+	err = vm.GetVmLogPath(com1LogPath)
+	if err != nil {
+		slog.Error("Com1Interactive", "err", err)
+		return err
+	}
 
+	vl, err := os.OpenFile(com1LogFile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		slog.Error("failed to open VM in log file", "err", err)
+	}
+	defer func(vl *os.File) {
+		_ = vl.Close()
+	}(vl)
+
+	for {
 		if vmInst.Status != "RUNNING" {
 			return nil
 		}
@@ -76,11 +123,15 @@ func (s *server) Com1Interactive(stream cirrina.VMInfo_Com1InteractiveServer) er
 			return err
 		}
 		inBytes := in.GetComInBytes()
-		//slog.Debug("Com1Interactive", "in", inBytes)
+		//slog.Debug("Com1Interactive", "inBytes", inBytes)
 		_, err = vmInst.Com1.Write(inBytes)
 		if err != nil {
 			return err
 		}
-
+		//slog.Debug("Com1Interactive", "inBytes", inBytes)
+		_, err = vl.Write(inBytes)
+		if err != nil {
+			return err
+		}
 	}
 }
