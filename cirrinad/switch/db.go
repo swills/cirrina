b21845ff211d9f48f28ca024c59e64f86807d466
Steve Wills <steve@mouf.net>
2023-08-10 09:58:59 -0400

    prevent color in logs

--- cirrinad/switch/db.go
+++ cirrinad/switch/db.go
@@ -4,7 +4,11 @@ import (
 	"cirrina/cirrinad/config"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+	"log"
+	"os"
 	"sync"
+	"time"
 )
 
 type singleton struct {
@@ -16,9 +20,20 @@ var instance *singleton
 var once sync.Once
 
 func getSwitchDb() *gorm.DB {
+
+	noColorLogger := logger.New(
+		log.New(os.Stdout, "\r\n", log.LstdFlags),
+		logger.Config{
+			SlowThreshold:             200 * time.Millisecond,
+			LogLevel:                  logger.Warn,
+			IgnoreRecordNotFoundError: false,
+			Colorful:                  false,
+		},
+	)
+
 	once.Do(func() {
 		instance = &singleton{}
-		switchDb, err := gorm.Open(sqlite.Open(config.Config.DB.Path), &gorm.Config{})
+		switchDb, err := gorm.Open(sqlite.Open(config.Config.DB.Path), &gorm.Config{Logger: noColorLogger})
 		if err != nil {
 			panic("failed to connect database")
 		}
--- cirrinad/vm/db.go
+++ cirrinad/vm/db.go
@@ -5,7 +5,11 @@ import (
 	"golang.org/x/exp/slog"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+	"log"
+	"os"
 	"sync"
+	"time"
 )
 
 type singleton struct {
@@ -17,9 +21,20 @@ var instance *singleton
 var once sync.Once
 
 func getVmDb() *gorm.DB {
+
+	noColorLogger := logger.New(
+		log.New(os.Stdout, "\r\n", log.LstdFlags),
+		logger.Config{
+			SlowThreshold:             200 * time.Millisecond,
+			LogLevel:                  logger.Warn,
+			IgnoreRecordNotFoundError: false,
+			Colorful:                  false,
+		},
+	)
+
 	once.Do(func() {
 		instance = &singleton{}
-		vmDb, err := gorm.Open(sqlite.Open(config.Config.DB.Path), &gorm.Config{})
+		vmDb, err := gorm.Open(sqlite.Open(config.Config.DB.Path), &gorm.Config{Logger: noColorLogger})
 		vmDb.Preload("Config")
 		if err != nil {
 			panic("failed to connect database")
