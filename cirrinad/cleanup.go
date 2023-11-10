87e08210afd8dd88d796afaa164473166d3acf8a
Steve Wills <steve@mouf.net>
2023-11-10 14:56:51 -0500

    refactor startup cleanup into separate file

---
+++ cirrinad/cleanup.go
@@ -0,0 +1,122 @@
+package main
+
+import (
+	"bytes"
+	"cirrina/cirrinad/config"
+	"cirrina/cirrinad/requests"
+	"cirrina/cirrinad/switch"
+	"cirrina/cirrinad/util"
+	"cirrina/cirrinad/vm"
+	"golang.org/x/exp/slog"
+	"golang.org/x/sys/execabs"
+	"net"
+	"syscall"
+	"time"
+)
+
+func cleanUpVms() {
+	vmList := vm.GetAll()
+	// deal with any leftover running VMs
+	for _, aVm := range vmList {
+		vmmPath := "/dev/vmm/" + aVm.Name
+		slog.Debug("checking VM", "name", aVm.Name, "path", vmmPath)
+		exists, err := util.PathExists(vmmPath)
+		if err != nil {
+			slog.Error("error checking VM", "err", err)
+			continue
+		}
+		if !exists {
+			continue
+		}
+		slog.Debug("leftover VM exists, checking pid", "name", aVm.Name, "pid", aVm.BhyvePid)
+		var pidStat bool
+		// check pid
+		if aVm.BhyvePid > 0 {
+			pidStat, err = util.PidExists(int(aVm.BhyvePid))
+			if err != nil {
+				slog.Error("error checking VM", "err", err)
+			}
+		}
+		if pidStat {
+			slog.Debug("leftover VM exists", "name", aVm.Name, "pid", aVm.BhyvePid, "maxWait", aVm.Config.MaxWait)
+			var sleptTime time.Duration
+			_ = syscall.Kill(int(aVm.BhyvePid), syscall.SIGTERM)
+			for {
+				pidStat, err = util.PidExists(int(aVm.BhyvePid))
+				if err != nil {
+					slog.Error("error checking VM", "err", err)
+					break
+				}
+				if !pidStat {
+					break
+				}
+				time.Sleep(10 * time.Millisecond)
+				sleptTime += 10 * time.Millisecond
+				if sleptTime > (time.Duration(aVm.Config.MaxWait) * time.Second) {
+					break
+				}
+			}
+			pidStillExists, err := util.PidExists(int(aVm.BhyvePid))
+			if err != nil {
+				slog.Error("error checking VM", "err", err)
+			} else {
+				if pidStillExists {
+					slog.Error("VM refused to die")
+				}
+			}
+		}
+		slog.Debug("destroying VM", "name", aVm.Name)
+		aVm.MaybeForceKillVM()
+	}
+
+	// clean up leftover nets and mark everything stopped
+	for _, aVm := range vmList {
+		slog.Debug("cleaning up VM net(s)", "name", aVm.Name)
+		aVm.NetCleanup()
+		slog.Debug("marking VM stopped", "name", aVm.Name)
+		aVm.SetStopped()
+	}
+}
+
+func cleanupNet() {
+	// destroy all the bridges we know about
+	_switch.DestroyBridges()
+
+	// look for network things in cirrinad group and destroy them
+	netInterfaces, err := net.Interfaces()
+	if err != nil {
+		panic(err)
+	}
+	slog.Debug("GetHostInterfaces", "netInterfaces", netInterfaces)
+	for _, inter := range netInterfaces {
+		intGroups, err := util.GetIntGroups(inter.Name)
+		if err != nil {
+			slog.Error("failed to get interface groups", "err", err)
+		}
+		if !util.ContainsStr(intGroups, "cirrinad") {
+			continue
+		}
+		slog.Debug("leftover interface found, destroying", "name", inter.Name)
+
+		cmd := execabs.Command(config.Config.Sys.Sudo, "/sbin/ifconfig", inter.Name, "destroy")
+		var out bytes.Buffer
+		cmd.Stdout = &out
+		if err := cmd.Start(); err != nil {
+			slog.Error("failed running ifconfig", "err", err, "out", out)
+		}
+		if err := cmd.Wait(); err != nil {
+			slog.Error("failed running ifconfig", "err", err, "out", out)
+		}
+	}
+}
+
+func cleanupDb() {
+	rowsCleared := requests.FailAllPending()
+	slog.Debug("cleared failed requests", "rowsCleared", rowsCleared)
+}
+
+func cleanupSystem() {
+	cleanUpVms()
+	cleanupNet()
+	cleanupDb()
+}
--- cirrinad/main.go
+++ cirrinad/main.go
@@ -1,10 +1,6 @@
 package main
 
 import (
-	"bytes"
-	"cirrina/cirrinad/requests"
-	exec "golang.org/x/sys/execabs"
-	"net"
 	"os"
 	"os/signal"
 	"runtime"
@@ -15,7 +11,6 @@ import (
 
 	"cirrina/cirrinad/config"
 	_switch "cirrina/cirrinad/switch"
-	"cirrina/cirrinad/util"
 	"cirrina/cirrinad/vm"
 
 	"golang.org/x/exp/slog"
@@ -81,107 +76,6 @@ func sigHandler(signal os.Signal) {
 	}
 }
 
-func cleanUpVms() {
-	vmList := vm.GetAll()
-	// deal with any leftover running VMs
-	for _, aVm := range vmList {
-		vmmPath := "/dev/vmm/" + aVm.Name
-		slog.Debug("checking VM", "name", aVm.Name, "path", vmmPath)
-		exists, err := util.PathExists(vmmPath)
-		if err != nil {
-			slog.Error("error checking VM", "err", err)
-			continue
-		}
-		if !exists {
-			continue
-		}
-		slog.Debug("leftover VM exists, checking pid", "name", aVm.Name, "pid", aVm.BhyvePid)
-		var pidStat bool
-		// check pid
-		if aVm.BhyvePid > 0 {
-			pidStat, err = util.PidExists(int(aVm.BhyvePid))
-			if err != nil {
-				slog.Error("error checking VM", "err", err)
-			}
-		}
-		if pidStat {
-			slog.Debug("leftover VM exists", "name", aVm.Name, "pid", aVm.BhyvePid, "maxWait", aVm.Config.MaxWait)
-			var sleptTime time.Duration
-			_ = syscall.Kill(int(aVm.BhyvePid), syscall.SIGTERM)
-			for {
-				pidStat, err = util.PidExists(int(aVm.BhyvePid))
-				if err != nil {
-					slog.Error("error checking VM", "err", err)
-					break
-				}
-				if !pidStat {
-					break
-				}
-				time.Sleep(10 * time.Millisecond)
-				sleptTime += 10 * time.Millisecond
-				if sleptTime > (time.Duration(aVm.Config.MaxWait) * time.Second) {
-					break
-				}
-			}
-			pidStillExists, err := util.PidExists(int(aVm.BhyvePid))
-			if err != nil {
-				slog.Error("error checking VM", "err", err)
-			} else {
-				if pidStillExists {
-					slog.Error("VM refused to die")
-				}
-			}
-		}
-		slog.Debug("destroying VM", "name", aVm.Name)
-		aVm.MaybeForceKillVM()
-	}
-
-	// clean up leftover nets and mark everything stopped
-	for _, aVm := range vmList {
-		slog.Debug("cleaning up VM net(s)", "name", aVm.Name)
-		aVm.NetCleanup()
-		slog.Debug("marking VM stopped", "name", aVm.Name)
-		aVm.SetStopped()
-	}
-}
-
-func cleanupNet() {
-	// destroy all the bridges we know about
-	_switch.DestroyBridges()
-
-	// look for network things in cirrinad group and destroy them
-	netInterfaces, err := net.Interfaces()
-	if err != nil {
-		panic(err)
-	}
-	slog.Debug("GetHostInterfaces", "netInterfaces", netInterfaces)
-	for _, inter := range netInterfaces {
-		intGroups, err := util.GetIntGroups(inter.Name)
-		if err != nil {
-			slog.Error("failed to get interface groups", "err", err)
-		}
-		if !util.ContainsStr(intGroups, "cirrinad") {
-			continue
-		}
-		slog.Debug("leftover interface found, destroying", "name", inter.Name)
-
-		cmd := exec.Command(config.Config.Sys.Sudo, "/sbin/ifconfig", inter.Name, "destroy")
-		var out bytes.Buffer
-		cmd.Stdout = &out
-		if err := cmd.Start(); err != nil {
-			slog.Error("failed running ifconfig", "err", err, "out", out)
-		}
-		if err := cmd.Wait(); err != nil {
-			slog.Error("failed running ifconfig", "err", err, "out", out)
-		}
-	}
-}
-
-func cleanupDb() {
-	rowsCleared := requests.FailAllPending()
-	slog.Debug("cleared failed requests", "rowsCleared", rowsCleared)
-}
-
 func main() {
 	signals := make(chan os.Signal)
 	signal.Notify(signals, os.Interrupt, syscall.SIGINFO)
@@ -222,14 +116,11 @@ func main() {
 	validateSystem()
 	slog.Debug("Finished host validation")
 	slog.Debug("Clean up starting")
-	cleanUpVms()
-	cleanupNet()
-	cleanupDb()
+	cleanupSystem()
 	slog.Debug("Clean up complete")
 
 	slog.Debug("Creating bridges")
 	_switch.CreateBridges()
-
 	slog.Info("Starting Daemon")
 
 	go vm.AutoStartVMs()
