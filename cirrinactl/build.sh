9db7945ea1e4f7706bbe45c000a68d54d86586e5
Steve Wills <steve@mouf.net>
2023-12-05 09:08:41 -0500

    add build scripts

---
+++ cirrinactl/build.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+GITINSTALLED=$(command git >/dev/null 2>&1; echo $?)
+
+if [ ${GITINSTALLED} -eq 1 ]; then
+  INGIT=$(git rev-parse --is-inside-work-tree)
+  if [ "${INGIT}" == "true" ]; then
+    VER=$(git rev-parse --short HEAD)
+    R=$(git diff-index --quiet HEAD -- ; echo $?)
+    if [ ${R} -ne 0 ]; then
+      VER=${VER}-dirty
+    fi  
+  else
+    VER="unknown"
+  fi  
+else
+  VER="unknown"
+fi
+
+rm cirrinactl ; go build -ldflags="-X cirrina/cirrinactl/cmd.mainVersion=${VER} -s -w -extldflags -static" . || exit 1
---
+++ cirrinad/build.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+
+# sqlite3 cirrina.sqlite .dump > my_vms_dump.sql
+
+#export CGO_ENABLED=0
+
+GITINSTALLED=$(command git >/dev/null 2>&1; echo $?)
+
+if [ ${GITINSTALLED} -eq 1 ]; then
+  INGIT=$(git rev-parse --is-inside-work-tree)
+  if [ "${INGIT}" == "true" ]; then
+    VER=$(git rev-parse --short HEAD)
+    R=$(git diff-index --quiet HEAD -- ; echo $?)
+    if [ ${R} -ne 0 ]; then
+      VER=${VER}-dirty
+    fi
+  else
+    VER="unknown"
+  fi
+else
+  VER="unknown"
+fi
+
+rm cirrinad ; go build -ldflags="-X main.mainVersion=${VER} -s -w -extldflags -static" .
