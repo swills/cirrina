e3637241053dad37b6e28e06a5885565efb9305a
Steve Wills <steve@mouf.net>
2024-05-19 11:58:21 -0400

    update

--- lint.sh
+++ lint.sh
@@ -6,12 +6,18 @@ cd ${SCRIPTDIR}
 
 . ./.venv/bin/activate
 
+export GOROOT=/usr/local/go121
+if [ ! -d /tmp/cirrinagopath ]; then
+  mkdir /tmp/cirrinagopath
+fi
 export GOPATH=/tmp/cirrinagopath
-export PATH=${PATH}:${GOPATH}/bin
-export GOMAXPROCS=12
+export PATH=${GOROOT}/bin:${PATH}:${GOPATH}/bin
+export GOMAXPROCS=$(sysctl -n hw.ncpu)
 
 idprio -t nice gci write --skip-generated --skip-vendor -s standard -s default -s 'prefix(cirrina)' cirrinad cirrinactl
 idprio -t nice pre-commit run --all-files
 
 idprio -t nice go test -v ./... -coverprofile=coverage.txt -covermode count -tags test > /dev/null
 idprio -t nice go tool cover -func coverage.txt | tail -n 1
+
+idprio -t nice go test -race ./cirrinad
