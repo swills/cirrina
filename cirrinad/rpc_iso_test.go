f7a1d928c3c5bb846093bd10cfd0e633705bc1c2
Steve Wills <steve@mouf.net>
2024-07-17 08:58:04 -0400

    add test for GetISOs

--- cirrinad/rpc_iso_test.go
+++ cirrinad/rpc_iso_test.go
@@ -1,1 +1,160 @@
 package main
+
+import (
+	"context"
+	"errors"
+	"io"
+	"log"
+	"net"
+	"regexp"
+	"testing"
+	"time"
+
+	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/go-test/deep"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/reflection"
+	"google.golang.org/grpc/resolver"
+	"google.golang.org/grpc/test/bufconn"
+	"gorm.io/gorm"
+
+	"cirrina/cirrina"
+	"cirrina/cirrinad/cirrinadtest"
+	"cirrina/cirrinad/iso"
+)
+
+func Test_server_GetISOs(t *testing.T) {
+	createUpdateTime := time.Now()
+
+	tests := []struct {
+		name        string
+		mockClosure func(testDB *gorm.DB, mock sqlmock.Sqlmock)
+		want        []string
+		wantErr     bool
+	}{
+		{
+			name: "Success",
+			mockClosure: func(testDB *gorm.DB, mock sqlmock.Sqlmock) {
+				iso.Instance = &iso.Singleton{ // prevents parallel testing
+					ISODB: testDB,
+				}
+
+				mock.ExpectQuery(
+					regexp.QuoteMeta(
+						"SELECT * FROM `isos` WHERE `isos`.`deleted_at` IS NULL",
+					),
+				).
+					WillReturnRows(
+						sqlmock.NewRows(
+							[]string{
+								"id",
+								"created_at",
+								"updated_at",
+								"deleted_at",
+								"name",
+								"description",
+								"path",
+								"size",
+								"checksum",
+							}).
+							AddRow(
+								"62bad068-bdaa-484f-9190-22d4f340f645",
+								createUpdateTime,
+								createUpdateTime,
+								nil,
+								"someRandomTestIsoForRpcTesting.iso",
+								"a totally made up iso",
+								"/bhyve/isos/someRandomTestIsoForRpcTesting.iso",
+								4621281280,
+								"326c7a07a393972d3fcd47deaa08e2b932d9298d96e9b4f63a17a2730f93384abc5feb1f511436dc91fcc8b6f56ed25b43dc91d9cdfc700d2655f7e35420d494", //nolint:lll
+							),
+					)
+			},
+			want: []string{"62bad068-bdaa-484f-9190-22d4f340f645"},
+		},
+	}
+
+	t.Parallel()
+
+	for _, testCase := range tests {
+		testCase := testCase
+		t.Run(testCase.name, func(t *testing.T) {
+			t.Parallel()
+
+			testDB, mock := cirrinadtest.NewMockDB("isoTest")
+
+			testCase.mockClosure(testDB, mock)
+
+			lis := bufconn.Listen(1024 * 1024)
+			s := grpc.NewServer()
+			reflection.Register(s)
+			cirrina.RegisterVMInfoServer(s, &server{})
+
+			go func() {
+				if err := s.Serve(lis); err != nil {
+					log.Fatalf("Server exited with error: %v", err)
+				}
+			}()
+
+			resolver.SetDefaultScheme("passthrough")
+
+			conn, err := grpc.NewClient("bufnet", grpc.WithContextDialer(func(context.Context, string) (net.Conn, error) {
+				return lis.Dial()
+			}), grpc.WithTransportCredentials(insecure.NewCredentials()))
+			if err != nil {
+				t.Fatalf("Failed to dial bufnet: %v", err)
+			}
+
+			defer func(conn *grpc.ClientConn) {
+				_ = conn.Close()
+			}(conn)
+
+			client := cirrina.NewVMInfoClient(conn)
+
+			var res cirrina.VMInfo_GetISOsClient
+
+			var got []string
+
+			var VMIso *cirrina.ISOID
+
+			ctx := context.Background()
+
+			res, err = client.GetISOs(ctx, &cirrina.ISOsQuery{})
+			if (err != nil) != testCase.wantErr {
+				t.Errorf("GetISOs() error = %v, wantErr %v", err, testCase.wantErr)
+			}
+
+			for {
+				VMIso, err = res.Recv()
+				if errors.Is(err, io.EOF) {
+					break
+				}
+
+				got = append(got, VMIso.GetValue())
+			}
+
+			diff := deep.Equal(got, testCase.want)
+			if diff != nil {
+				t.Errorf("compare failed: %v", diff)
+			}
+
+			mock.ExpectClose()
+
+			db, err := testDB.DB()
+			if err != nil {
+				t.Error(err)
+			}
+
+			err = db.Close()
+			if err != nil {
+				t.Error(err)
+			}
+
+			err = mock.ExpectationsWereMet()
+			if err != nil {
+				t.Errorf("there were unfulfilled expectations: %s", err)
+			}
+		})
+	}
+}
