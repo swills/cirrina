0d85fb658f33a1580ee51f3dce862f665e33e185
Steve Wills <steve@mouf.net>
2024-08-11 19:57:25 -0400

    add test for SetVNCPort

--- cirrinad/vm/db_test.go
+++ cirrinad/vm/db_test.go
@@ -1233,3 +1233,208 @@ func TestVM_SetDebugPort(t *testing.T) {
 		})
 	}
 }
+
+func TestVM_SetVNCPort(t *testing.T) {
+	createUpdateTime := time.Now()
+
+	type fields struct {
+		ID          string
+		CreatedAt   time.Time
+		UpdatedAt   time.Time
+		Name        string
+		Description string
+		Status      StatusType
+		BhyvePid    uint32
+		VNCPort     int32
+		DebugPort   int32
+		Config      Config
+		ISOs        []*iso.ISO
+		Disks       []*disk.Disk
+		Com1Dev     string
+		Com2Dev     string
+		Com3Dev     string
+		Com4Dev     string
+		Com1write   bool
+		Com2write   bool
+		Com3write   bool
+		Com4write   bool
+	}
+
+	type args struct {
+		port int
+	}
+
+	tests := []struct {
+		name          string
+		mockVMClosure func(testDB *gorm.DB, mock sqlmock.Sqlmock)
+		fields        fields
+		args          args
+	}{
+		{
+			name: "SuccessSpecific",
+			mockVMClosure: func(testDB *gorm.DB, mock sqlmock.Sqlmock) {
+				Instance = &singleton{ // prevents parallel testing
+					vmDB: testDB,
+				}
+
+				mock.ExpectBegin()
+				mock.ExpectExec(
+					regexp.QuoteMeta("UPDATE `configs` SET `com1`=?,`com2`=?,`com3`=?,`acpi`=?,`auto_start`=?,`auto_start_delay`=?,`com1_dev`=?,`com1_log`=?,`com1_speed`=?,`com2_dev`=?,`com2_log`=?,`com2_speed`=?,`com3_dev`=?,`com3_log`=?,`com3_speed`=?,`com4`=?,`com4_dev`=?,`com4_log`=?,`com4_speed`=?,`cpu`=?,`debug`=?,`debug_port`=?,`debug_wait`=?,`destroy_power_off`=?,`exit_on_pause`=?,`extra_args`=?,`host_bridge`=?,`ignore_unknown_msr`=?,`kbd_layout`=?,`max_wait`=?,`mem`=?,`pcpu`=?,`priority`=?,`protect`=?,`rbps`=?,`restart`=?,`restart_delay`=?,`riops`=?,`screen`=?,`screen_height`=?,`screen_width`=?,`sound`=?,`sound_in`=?,`sound_out`=?,`store_uefi_vars`=?,`tablet`=?,`use_hlt`=?,`utc_time`=?,`vnc_port`=?,`vnc_wait`=?,`wbps`=?,`wiops`=?,`wire_guest_mem`=?,`updated_at`=? WHERE `configs`.`deleted_at` IS NULL AND `id` = ?"), //nolint:lll
+				).
+					WithArgs(true, false, false, true, false, 60, "AUTO", false, 115200, "AUTO", false, 115200, "AUTO", false, 115200, false, "AUTO", false, 115200, 2, false, "AUTO", false, true, true, "", true, true, "default", 60, 2048, 0, 0, nil, 0, true, 0, 0, true, 1080, 1920, false, "/dev/dsp0", "/dev/dsp0", true, true, true, true, "AUTO", false, 0, 0, false, sqlmock.AnyArg(), 81). //nolint:lll
+					WillReturnResult(sqlmock.NewResult(1, 1))
+				mock.ExpectCommit()
+
+				mock.ExpectBegin()
+				mock.ExpectExec(
+					regexp.QuoteMeta(
+						"UPDATE `vms` SET `com1_dev`=?,`com2_dev`=?,`com3_dev`=?,`com4_dev`=?,`debug_port`=?,`description`=?,`name`=?,`vnc_port`=?,`updated_at`=? WHERE `vms`.`deleted_at` IS NULL AND `id` = ?"), //nolint:lll
+				).
+					WithArgs("", "", "", "", 0, "test vm", "test2024081103", 6901, sqlmock.AnyArg(), "2a8a07a1-0fd0-41cd-a4ce-e104c4b9f136"). //nolint:lll
+					WillReturnResult(sqlmock.NewResult(1, 1))
+				mock.ExpectCommit()
+
+				mock.ExpectExec(
+					regexp.QuoteMeta("DELETE FROM `vm_isos` WHERE `vm_id` = ?"),
+				).
+					WithArgs("2a8a07a1-0fd0-41cd-a4ce-e104c4b9f136").
+					// does not matter how many rows are returned, we wipe all isos from the VM
+					// unconditionally and add the ones we want to have
+					WillReturnResult(sqlmock.NewResult(1, 27))
+
+				mock.ExpectBegin()
+				mock.ExpectExec(
+					regexp.QuoteMeta("INSERT INTO `vm_isos` (`vm_id`,`iso_id`, `position`) VALUES (?,?,?)"),
+				).
+					WithArgs("2a8a07a1-0fd0-41cd-a4ce-e104c4b9f136", "4876c282-9490-46c3-b034-0c7b0111020a", 0).
+					WillReturnResult(sqlmock.NewResult(1, 1))
+				mock.ExpectCommit()
+
+				mock.ExpectExec(
+					regexp.QuoteMeta("DELETE FROM `vm_disks` WHERE `vm_id` = ?"),
+				).
+					WithArgs("2a8a07a1-0fd0-41cd-a4ce-e104c4b9f136").
+					WillReturnResult(sqlmock.NewResult(1, 1))
+
+				mock.ExpectBegin()
+				mock.ExpectExec(
+					regexp.QuoteMeta("INSERT INTO `vm_disks` (`vm_id`,`disk_id`, `position`) VALUES (?,?,?)"),
+				).
+					WithArgs("2a8a07a1-0fd0-41cd-a4ce-e104c4b9f136", "742a91b4-96b8-4196-97f1-8df461163ff7", 0).
+					WillReturnResult(sqlmock.NewResult(1, 1))
+				mock.ExpectCommit()
+			},
+			fields: fields{
+				ID:          "2a8a07a1-0fd0-41cd-a4ce-e104c4b9f136",
+				CreatedAt:   createUpdateTime,
+				UpdatedAt:   createUpdateTime,
+				Name:        "test2024081103",
+				Description: "test vm",
+				Status:      "STOPPED",
+				Config: Config{
+					Model: gorm.Model{
+						ID:        81,
+						CreatedAt: createUpdateTime,
+						UpdatedAt: createUpdateTime,
+					},
+					VMID:             "2a8a07a1-0fd0-41cd-a4ce-e104c4b9f136",
+					CPU:              2,
+					Mem:              2048,
+					MaxWait:          60,
+					Restart:          true,
+					Screen:           true,
+					ScreenWidth:      1920,
+					ScreenHeight:     1080,
+					Sound:            false,
+					SoundIn:          "/dev/dsp0",
+					SoundOut:         "/dev/dsp0",
+					VNCPort:          "AUTO",
+					Tablet:           true,
+					StoreUEFIVars:    true,
+					UTCTime:          true,
+					HostBridge:       true,
+					ACPI:             true,
+					UseHLT:           true,
+					ExitOnPause:      true,
+					DestroyPowerOff:  true,
+					IgnoreUnknownMSR: true,
+					KbdLayout:        "default",
+					Com1:             true,
+					Com1Dev:          "AUTO",
+					Com2Dev:          "AUTO",
+					Com3Dev:          "AUTO",
+					Com4Dev:          "AUTO",
+					Com1Speed:        115200,
+					Com2Speed:        115200,
+					Com3Speed:        115200,
+					Com4Speed:        115200,
+					AutoStartDelay:   60,
+					DebugPort:        "AUTO",
+				},
+				ISOs: []*iso.ISO{
+					{
+						ID: "4876c282-9490-46c3-b034-0c7b0111020a",
+					},
+				},
+				Disks: []*disk.Disk{
+					{
+						ID: "742a91b4-96b8-4196-97f1-8df461163ff7",
+					},
+				},
+			},
+			args: args{
+				port: 6901,
+			},
+		},
+	}
+
+	for _, testCase := range tests {
+		testCase := testCase
+		t.Run(testCase.name, func(t *testing.T) {
+			testDB, mock := cirrinadtest.NewMockDB("vmTest")
+			testCase.mockVMClosure(testDB, mock)
+
+			testVM := &VM{
+				ID:          testCase.fields.ID,
+				CreatedAt:   testCase.fields.CreatedAt,
+				UpdatedAt:   testCase.fields.UpdatedAt,
+				Name:        testCase.fields.Name,
+				Description: testCase.fields.Description,
+				Status:      testCase.fields.Status,
+				BhyvePid:    testCase.fields.BhyvePid,
+				VNCPort:     testCase.fields.VNCPort,
+				DebugPort:   testCase.fields.DebugPort,
+				Config:      testCase.fields.Config,
+				ISOs:        testCase.fields.ISOs,
+				Disks:       testCase.fields.Disks,
+				Com1Dev:     testCase.fields.Com1Dev,
+				Com2Dev:     testCase.fields.Com2Dev,
+				Com3Dev:     testCase.fields.Com3Dev,
+				Com4Dev:     testCase.fields.Com4Dev,
+				Com1write:   testCase.fields.Com1write,
+				Com2write:   testCase.fields.Com2write,
+				Com3write:   testCase.fields.Com3write,
+				Com4write:   testCase.fields.Com4write,
+			}
+
+			testVM.SetVNCPort(testCase.args.port)
+
+			mock.ExpectClose()
+
+			db, err := testDB.DB()
+			if err != nil {
+				t.Error(err)
+			}
+
+			err = db.Close()
+			if err != nil {
+				t.Error(err)
+			}
+
+			err = mock.ExpectationsWereMet()
+			if err != nil {
+				t.Errorf("there were unfulfilled expectations: %s", err)
+			}
+		})
+	}
+}
