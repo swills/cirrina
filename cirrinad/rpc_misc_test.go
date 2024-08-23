67ee0c678ddebb8dc98c26356eeeeb6df282e0d7
Steve Wills <steve@mouf.net>
2024-08-23 14:28:13 -0400

    add test for RequestStatus

--- cirrinad/rpc_misc_test.go
+++ cirrinad/rpc_misc_test.go
@@ -6,8 +6,11 @@ import (
 	"io"
 	"log"
 	"net"
+	"regexp"
 	"testing"
+	"time"
 
+	"github.com/DATA-DOG/go-sqlmock"
 	"github.com/go-test/deep"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
@@ -15,8 +18,11 @@ import (
 	"google.golang.org/grpc/resolver"
 	"google.golang.org/grpc/test/bufconn"
 	"google.golang.org/protobuf/types/known/wrapperspb"
+	"gorm.io/gorm"
 
 	"cirrina/cirrina"
+	"cirrina/cirrinad/cirrinadtest"
+	"cirrina/cirrinad/requests"
 	"cirrina/cirrinad/util"
 )
 
@@ -167,6 +173,253 @@ func Test_server_GetNetInterfaces(t *testing.T) {
 			diff := deep.Equal(got, testCase.want)
 			if diff != nil {
 				t.Errorf("compare failed: %v", diff)
+			}
+		})
+	}
+}
+
+//nolint:paralleltest
+func Test_server_RequestStatus(t *testing.T) {
+	createUpdateTime := time.Now()
+
+	type args struct {
+		requestID *cirrina.RequestID
+	}
+
+	tests := []struct {
+		name        string
+		args        args
+		mockClosure func(testDB *gorm.DB, mock sqlmock.Sqlmock)
+		want        *cirrina.ReqStatus
+		wantErr     bool
+	}{
+		{
+			name: "SuccessCompleteSuccessfulReq",
+			mockClosure: func(testDB *gorm.DB, mock sqlmock.Sqlmock) {
+				requests.Instance = &requests.Singleton{ReqDB: testDB}
+
+				mock.ExpectQuery(
+					regexp.QuoteMeta(
+						"SELECT * FROM `requests` WHERE `requests`.`id` = ? AND `requests`.`deleted_at` IS NULL LIMIT 1",
+					),
+				).
+					WithArgs("8521cc16-a501-483a-a741-6e01c2789e1d").
+					WillReturnRows(sqlmock.NewRows([]string{
+						"id",
+						"created_at",
+						"updated_at",
+						"deleted_at",
+						"successful",
+						"complete",
+						"type",
+					}).
+						AddRow(
+							"8521cc16-a501-483a-a741-6e01c2789e1d",
+							createUpdateTime,
+							createUpdateTime,
+							nil,
+							"1",
+							"1",
+							"START",
+						),
+					)
+			},
+			args: args{
+				requestID: &cirrina.RequestID{
+					Value: "8521cc16-a501-483a-a741-6e01c2789e1d",
+				},
+			},
+			want: func() *cirrina.ReqStatus {
+				r := cirrina.ReqStatus{
+					Complete: true,
+					Success:  true,
+				}
+
+				return &r
+			}(),
+			wantErr: false,
+		},
+		{
+			name: "SuccessNotCompleteReq",
+			mockClosure: func(testDB *gorm.DB, mock sqlmock.Sqlmock) {
+				requests.Instance = &requests.Singleton{ReqDB: testDB}
+
+				mock.ExpectQuery(
+					regexp.QuoteMeta(
+						"SELECT * FROM `requests` WHERE `requests`.`id` = ? AND `requests`.`deleted_at` IS NULL LIMIT 1",
+					),
+				).
+					WithArgs("8521cc16-a501-483a-a741-6e01c2789e1d").
+					WillReturnRows(sqlmock.NewRows([]string{
+						"id",
+						"created_at",
+						"updated_at",
+						"deleted_at",
+						"successful",
+						"complete",
+						"type",
+					}).
+						AddRow(
+							"8521cc16-a501-483a-a741-6e01c2789e1d",
+							createUpdateTime,
+							createUpdateTime,
+							nil,
+							"0",
+							"0",
+							"START",
+						),
+					)
+			},
+			args: args{
+				requestID: &cirrina.RequestID{
+					Value: "8521cc16-a501-483a-a741-6e01c2789e1d",
+				},
+			},
+			want: func() *cirrina.ReqStatus {
+				r := cirrina.ReqStatus{
+					Complete: false,
+					Success:  false,
+				}
+
+				return &r
+			}(),
+			wantErr: false,
+		},
+		{
+			name: "ErrorNoId",
+			mockClosure: func(testDB *gorm.DB, mock sqlmock.Sqlmock) {
+				requests.Instance = &requests.Singleton{ReqDB: testDB}
+
+				mock.ExpectQuery(
+					regexp.QuoteMeta(
+						"SELECT * FROM `requests` WHERE `requests`.`id` = ? AND `requests`.`deleted_at` IS NULL LIMIT 1",
+					),
+				).
+					WithArgs("8521cc16-a501-483a-a741-6e01c2789e1d").
+					WillReturnRows(sqlmock.NewRows([]string{
+						"id",
+						"created_at",
+						"updated_at",
+						"deleted_at",
+						"successful",
+						"complete",
+						"type",
+					}).
+						AddRow(
+							"",
+							createUpdateTime,
+							createUpdateTime,
+							nil,
+							"1",
+							"1",
+							"START",
+						),
+					)
+			},
+			args: args{
+				requestID: &cirrina.RequestID{
+					Value: "8521cc16-a501-483a-a741-6e01c2789e1d",
+				},
+			},
+			want:    nil,
+			wantErr: true,
+		},
+		{
+			name: "ErrorReqNotFound",
+			mockClosure: func(testDB *gorm.DB, mock sqlmock.Sqlmock) {
+				requests.Instance = &requests.Singleton{ReqDB: testDB}
+
+				mock.ExpectQuery(
+					regexp.QuoteMeta(
+						"SELECT * FROM `requests` WHERE `requests`.`id` = ? AND `requests`.`deleted_at` IS NULL LIMIT 1",
+					),
+				).
+					WithArgs("d8d8d383-280e-4c44-8891-efe127c9b0cf")
+			},
+			args: args{
+				requestID: &cirrina.RequestID{
+					Value: "d8d8d383-280e-4c44-8891-efe127c9b0cf",
+				},
+			},
+			want:    nil,
+			wantErr: true,
+		},
+		{
+			name: "ErrorBadID",
+			mockClosure: func(testDB *gorm.DB, _ sqlmock.Sqlmock) {
+				requests.Instance = &requests.Singleton{ReqDB: testDB}
+			},
+			args: args{
+				requestID: &cirrina.RequestID{
+					Value: "8521cc16-a501-483a-",
+				},
+			},
+			want:    nil,
+			wantErr: true,
+		},
+	}
+
+	for _, testCase := range tests {
+		t.Run(testCase.name, func(t *testing.T) {
+			testDB, mock := cirrinadtest.NewMockDB("testDB")
+
+			testCase.mockClosure(testDB, mock)
+
+			lis := bufconn.Listen(1024 * 1024)
+			s := grpc.NewServer()
+			reflection.Register(s)
+			cirrina.RegisterVMInfoServer(s, &server{})
+
+			go func() {
+				if err := s.Serve(lis); err != nil {
+					log.Fatalf("Server exited with error: %v", err)
+				}
+			}()
+
+			resolver.SetDefaultScheme("passthrough")
+
+			conn, err := grpc.NewClient("bufnet", grpc.WithContextDialer(func(context.Context, string) (net.Conn, error) {
+				return lis.Dial()
+			}), grpc.WithTransportCredentials(insecure.NewCredentials()))
+			if err != nil {
+				t.Fatalf("Failed to dial bufnet: %v", err)
+			}
+
+			defer func(conn *grpc.ClientConn) {
+				_ = conn.Close()
+			}(conn)
+
+			client := cirrina.NewVMInfoClient(conn)
+
+			var got *cirrina.ReqStatus
+
+			got, err = client.RequestStatus(context.Background(), testCase.args.requestID)
+			if (err != nil) != testCase.wantErr {
+				t.Errorf("RequestStatus() error = %v, wantErr %v", err, testCase.wantErr)
+
+				return
+			}
+
+			diff := deep.Equal(got, testCase.want)
+			if diff != nil {
+				t.Errorf("compare failed: %v", diff)
+			}
+
+			mock.ExpectClose()
+
+			db, err := testDB.DB()
+			if err != nil {
+				t.Error(err)
+			}
+
+			err = db.Close()
+			if err != nil {
+				t.Error(err)
+			}
+
+			err = mock.ExpectationsWereMet()
+			if err != nil {
+				t.Errorf("there were unfulfilled expectations: %s", err)
 			}
 		})
 	}
