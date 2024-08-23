20f5eb7d695ea9748455e4391be66eb0d6a5b834
Steve Wills <steve@mouf.net>
2024-08-23 13:53:54 -0400

    add test for GetNetInterfaces

--- cirrinad/rpc_misc_test.go
+++ cirrinad/rpc_misc_test.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"context"
+	"errors"
+	"io"
 	"log"
 	"net"
 	"testing"
@@ -15,6 +17,7 @@ import (
 	"google.golang.org/protobuf/types/known/wrapperspb"
 
 	"cirrina/cirrina"
+	"cirrina/cirrinad/util"
 )
 
 //nolint:paralleltest
@@ -85,5 +88,136 @@ func Test_server_GetVersion(t *testing.T) {
 				t.Errorf("compare failed: %v", diff)
 			}
 		})
+	}
+}
+
+//nolint:paralleltest
+func Test_server_GetNetInterfaces(t *testing.T) {
+	tests := []struct {
+		name                string
+		hostIntStubFunc     func() ([]net.Interface, error)
+		getIntGroupStubFunc func(string) ([]string, error)
+		want                []string
+		wantErr             bool
+	}{
+		{
+			name:                "Success",
+			hostIntStubFunc:     StubHostInterfacesSuccess3,
+			getIntGroupStubFunc: StubGetHostIntGroupSuccess3,
+			want:                []string{"re0", "re1", "re2"},
+			wantErr:             false,
+		},
+	}
+
+	for _, testCase := range tests {
+		t.Run(testCase.name, func(t *testing.T) {
+			util.NetInterfacesFunc = testCase.hostIntStubFunc
+
+			t.Cleanup(func() { util.NetInterfacesFunc = net.Interfaces })
+
+			util.GetIntGroupsFunc = testCase.getIntGroupStubFunc
+
+			lis := bufconn.Listen(1024 * 1024)
+			s := grpc.NewServer()
+			reflection.Register(s)
+			cirrina.RegisterVMInfoServer(s, &server{})
+
+			go func() {
+				if err := s.Serve(lis); err != nil {
+					log.Fatalf("Server exited with error: %v", err)
+				}
+			}()
+
+			resolver.SetDefaultScheme("passthrough")
+
+			conn, err := grpc.NewClient("bufnet", grpc.WithContextDialer(func(context.Context, string) (net.Conn, error) {
+				return lis.Dial()
+			}), grpc.WithTransportCredentials(insecure.NewCredentials()))
+			if err != nil {
+				t.Fatalf("Failed to dial bufnet: %v", err)
+			}
+
+			defer func(conn *grpc.ClientConn) {
+				_ = conn.Close()
+			}(conn)
+
+			client := cirrina.NewVMInfoClient(conn)
+
+			var res cirrina.VMInfo_GetNetInterfacesClient
+
+			var NetIf *cirrina.NetIf
+
+			var got []string
+
+			res, err = client.GetNetInterfaces(context.Background(), &cirrina.NetInterfacesReq{})
+
+			if (err != nil) != testCase.wantErr {
+				t.Errorf("GetNetInterfaces() error = %v, wantErr %v", err, testCase.wantErr)
+			}
+
+			for {
+				NetIf, err = res.Recv()
+				if errors.Is(err, io.EOF) {
+					break
+				}
+
+				got = append(got, NetIf.GetInterfaceName())
+			}
+
+			diff := deep.Equal(got, testCase.want)
+			if diff != nil {
+				t.Errorf("compare failed: %v", diff)
+			}
+		})
+	}
+}
+
+// test helpers from here down
+
+func StubHostInterfacesSuccess3() ([]net.Interface, error) {
+	return []net.Interface{
+		{
+			Index:        1,
+			MTU:          1500,
+			Name:         "re0",
+			HardwareAddr: net.HardwareAddr{0xff, 0xdd, 0xcc, 0x28, 0x73, 0x3e},
+			Flags:        0x33,
+		},
+		{
+			Index:        2,
+			MTU:          16384,
+			Name:         "lo0",
+			HardwareAddr: net.HardwareAddr(nil),
+			Flags:        0x35,
+		},
+		{
+			Index:        3,
+			MTU:          1500,
+			Name:         "re1",
+			HardwareAddr: net.HardwareAddr{0xff, 0xdd, 0xcc, 0x91, 0x7a, 0x71},
+			Flags:        0x33,
+		},
+		{
+			Index:        4,
+			MTU:          1500,
+			Name:         "re2",
+			HardwareAddr: net.HardwareAddr{0xab, 0xcd, 0xef, 0x01, 0x23, 0x34},
+			Flags:        0x33,
+		},
+	}, nil
+}
+
+func StubGetHostIntGroupSuccess3(intName string) ([]string, error) {
+	switch intName {
+	case "re0":
+		return []string{}, nil
+	case "re1":
+		return []string{}, nil
+	case "re2":
+		return []string{}, nil
+	case "lo0":
+		return []string{"lo"}, nil
+	default:
+		return nil, nil
 	}
 }
