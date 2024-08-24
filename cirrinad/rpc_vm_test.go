85aa59e2b33e21f456981d724679aecc89f677a3
Steve Wills <steve@mouf.net>
2024-08-23 20:51:01 -0400

    add test for GetVMState

--- cirrinad/rpc_vm_test.go
+++ cirrinad/rpc_vm_test.go
@@ -303,3 +303,305 @@ func Test_server_GetVMName(t *testing.T) {
 		})
 	}
 }
+
+//nolint:paralleltest,maintidx
+func Test_server_GetVMState(t *testing.T) {
+	type args struct {
+		vmID *cirrina.VMID
+	}
+
+	tests := []struct {
+		name        string
+		mockClosure func()
+		args        args
+		want        *cirrina.VMState
+		wantErr     bool
+	}{
+		{
+			name: "SuccessStopped",
+			mockClosure: func() {
+				testVM1 := vm.VM{
+					ID:   "176af73d-e4ad-4b55-adf9-f21dc0d68d66",
+					Name: "test2024082303",
+					Config: vm.Config{
+						Model: gorm.Model{
+							ID: 339,
+						},
+						VMID: "176af73d-e4ad-4b55-adf9-f21dc0d68d66",
+						CPU:  2,
+						Mem:  1024,
+					},
+					Status:    vm.STOPPED,
+					VNCPort:   0,
+					DebugPort: 0,
+				}
+				vm.List.VMList[testVM1.ID] = &testVM1
+			},
+			args: args{
+				vmID: &cirrina.VMID{
+					Value: "176af73d-e4ad-4b55-adf9-f21dc0d68d66",
+				},
+			},
+			want: func() *cirrina.VMState {
+				vmState := cirrina.VMState{
+					Status:    cirrina.VmStatus_STATUS_STOPPED,
+					VncPort:   0,
+					DebugPort: 0,
+				}
+
+				return &vmState
+			}(),
+			wantErr: false,
+		},
+		{
+			name: "SuccessStarting",
+			mockClosure: func() {
+				testVM1 := vm.VM{
+					ID:   "f91e68e2-716d-4496-b55b-d6a2b6121388",
+					Name: "test2024082303",
+					Config: vm.Config{
+						Model: gorm.Model{
+							ID: 339,
+						},
+						VMID: "f91e68e2-716d-4496-b55b-d6a2b6121388",
+						CPU:  2,
+						Mem:  1024,
+					},
+					Status:    vm.STARTING,
+					VNCPort:   6900,
+					DebugPort: 3434,
+				}
+				vm.List.VMList[testVM1.ID] = &testVM1
+			},
+			args: args{
+				vmID: &cirrina.VMID{
+					Value: "f91e68e2-716d-4496-b55b-d6a2b6121388",
+				},
+			},
+			want: func() *cirrina.VMState {
+				vmState := cirrina.VMState{
+					Status:    cirrina.VmStatus_STATUS_STARTING,
+					VncPort:   6900,
+					DebugPort: 3434,
+				}
+
+				return &vmState
+			}(),
+			wantErr: false,
+		},
+		{
+			name: "SuccessRunning",
+			mockClosure: func() {
+				testVM1 := vm.VM{
+					ID:   "a5277e49-6cc0-49a5-a492-6447dd094e4f",
+					Name: "test2024082303",
+					Config: vm.Config{
+						Model: gorm.Model{
+							ID: 339,
+						},
+						VMID: "a5277e49-6cc0-49a5-a492-6447dd094e4f",
+						CPU:  2,
+						Mem:  1024,
+					},
+					Status:    vm.RUNNING,
+					VNCPort:   6901,
+					DebugPort: 3435,
+				}
+				vm.List.VMList[testVM1.ID] = &testVM1
+			},
+			args: args{
+				vmID: &cirrina.VMID{
+					Value: "a5277e49-6cc0-49a5-a492-6447dd094e4f",
+				},
+			},
+			want: func() *cirrina.VMState {
+				vmState := cirrina.VMState{
+					Status:    cirrina.VmStatus_STATUS_RUNNING,
+					VncPort:   6901,
+					DebugPort: 3435,
+				}
+
+				return &vmState
+			}(),
+			wantErr: false,
+		},
+		{
+			name: "SuccessStopping",
+			mockClosure: func() {
+				testVM1 := vm.VM{
+					ID:   "0160587e-62af-4166-8e60-47f32d6e481f",
+					Name: "test2024082303",
+					Config: vm.Config{
+						Model: gorm.Model{
+							ID: 339,
+						},
+						VMID: "0160587e-62af-4166-8e60-47f32d6e481f",
+						CPU:  2,
+						Mem:  1024,
+					},
+					Status:    vm.STOPPING,
+					VNCPort:   6902,
+					DebugPort: 3436,
+				}
+				vm.List.VMList[testVM1.ID] = &testVM1
+			},
+			args: args{
+				vmID: &cirrina.VMID{
+					Value: "0160587e-62af-4166-8e60-47f32d6e481f",
+				},
+			},
+			want: func() *cirrina.VMState {
+				vmState := cirrina.VMState{
+					Status:    cirrina.VmStatus_STATUS_STOPPING,
+					VncPort:   6902,
+					DebugPort: 3436,
+				}
+
+				return &vmState
+			}(),
+			wantErr: false,
+		},
+		{
+			name: "BadStatus",
+			mockClosure: func() {
+				testVM1 := vm.VM{
+					ID:   "176af73d-e4ad-4b55-adf9-f21dc0d68d66",
+					Name: "test2024082303",
+					Config: vm.Config{
+						Model: gorm.Model{
+							ID: 339,
+						},
+						VMID: "176af73d-e4ad-4b55-adf9-f21dc0d68d66",
+						CPU:  2,
+						Mem:  1024,
+					},
+					Status:    vm.StatusType("junk"),
+					VNCPort:   0,
+					DebugPort: 0,
+				}
+				vm.List.VMList[testVM1.ID] = &testVM1
+			},
+			args: args{
+				vmID: &cirrina.VMID{
+					Value: "176af73d-e4ad-4b55-adf9-f21dc0d68d66",
+				},
+			},
+			want:    nil,
+			wantErr: true,
+		},
+		{
+			name: "EmptyName",
+			mockClosure: func() {
+				testVM1 := vm.VM{
+					ID:   "1f09210f-258f-4765-ab88-24f9dccb61e1",
+					Name: "",
+					Config: vm.Config{
+						Model: gorm.Model{
+							ID: 339,
+						},
+						VMID: "1f09210f-258f-4765-ab88-24f9dccb61e1",
+						CPU:  2,
+						Mem:  1024,
+					},
+					Status:    vm.STOPPED,
+					VNCPort:   0,
+					DebugPort: 0,
+				}
+				vm.List.VMList[testVM1.ID] = &testVM1
+			},
+			args: args{
+				vmID: &cirrina.VMID{
+					Value: "1f09210f-258f-4765-ab88-24f9dccb61e1",
+				},
+			},
+			want:    nil,
+			wantErr: true,
+		},
+		{
+			name: "NotFound",
+			mockClosure: func() {
+				testVM1 := vm.VM{
+					ID:   "176af73d-e4ad-4b55-adf9-f21dc0d68d66",
+					Name: "test2024082303",
+					Config: vm.Config{
+						Model: gorm.Model{
+							ID: 339,
+						},
+						VMID: "176af73d-e4ad-4b55-adf9-f21dc0d68d66",
+						CPU:  2,
+						Mem:  1024,
+					},
+					Status:    vm.STOPPED,
+					VNCPort:   0,
+					DebugPort: 0,
+				}
+				vm.List.VMList[testVM1.ID] = &testVM1
+			},
+			args: args{
+				vmID: &cirrina.VMID{
+					Value: "401c0bca-274d-486f-a1f3-4e95ba8c268f",
+				},
+			},
+			want:    nil,
+			wantErr: true,
+		},
+		{
+			name: "BadUuid",
+			mockClosure: func() {
+			},
+			args: args{
+				vmID: &cirrina.VMID{
+					Value: "176af73d-e4ad-4b55-adf9-f21dc0",
+				},
+			},
+			want:    nil,
+			wantErr: true,
+		},
+	}
+
+	for _, testCase := range tests {
+		t.Run(testCase.name, func(t *testing.T) {
+			testCase.mockClosure()
+
+			lis := bufconn.Listen(1024 * 1024)
+			s := grpc.NewServer()
+			reflection.Register(s)
+			cirrina.RegisterVMInfoServer(s, &server{})
+
+			go func() {
+				if err := s.Serve(lis); err != nil {
+					log.Fatalf("Server exited with error: %v", err)
+				}
+			}()
+
+			resolver.SetDefaultScheme("passthrough")
+
+			conn, err := grpc.NewClient("bufnet", grpc.WithContextDialer(func(context.Context, string) (net.Conn, error) {
+				return lis.Dial()
+			}), grpc.WithTransportCredentials(insecure.NewCredentials()))
+			if err != nil {
+				t.Fatalf("Failed to dial bufnet: %v", err)
+			}
+
+			defer func(conn *grpc.ClientConn) {
+				_ = conn.Close()
+			}(conn)
+
+			client := cirrina.NewVMInfoClient(conn)
+
+			var got *cirrina.VMState
+
+			got, err = client.GetVMState(context.Background(), testCase.args.vmID)
+			if (err != nil) != testCase.wantErr {
+				t.Errorf("GetVMState() error = %v, wantErr %v", err, testCase.wantErr)
+
+				return
+			}
+
+			diff := deep.Equal(got, testCase.want)
+			if diff != nil {
+				t.Errorf("compare failed: %v", diff)
+			}
+		})
+	}
+}
