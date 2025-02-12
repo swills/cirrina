58d9113c9860a6868b22fd77b51017f9ddcf8d4e
Steve Wills <steve@mouf.net>
2025-02-12 00:43:49 -0500

    add some tools to tools.go

--- tools/tools.go
+++ tools/tools.go
@@ -3,7 +3,13 @@
 
 package tools
 
+//go:generate go run protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative cirrina.proto
+
 import (
-	_ "go.uber.org/mock/mockgen"
 	_ "github.com/a-h/templ/cmd/templ"
+	_ "github.com/daixiang0/gci"
+	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
+	_ "go.uber.org/mock/mockgen"
+	_ "google.golang.org/grpc/cmd/protoc-gen-go-grpc"
+	_ "google.golang.org/protobuf/cmd/protoc-gen-go"
 )
